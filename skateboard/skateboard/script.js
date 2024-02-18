8868d486ab9f8123b6422456c3cf10f5dcc927fa
papopapo1 <145740716+papopapo1@users.noreply.github.com>
2024-02-18 17:00:45 +0700

    Add files via upload

---
+++ skateboard/skateboard/script.js
@@ -0,0 +1,20 @@
+const body = document.getElementById("body");
+const slider = document.getElementById("slider");
+
+window.onload = function() {
+  if (localStorage.getItem('darkMode') == "true") {
+    body.classList.toggle("darkMode");
+    slider.checked = true;
+  }
+}
+
+function changeTheme() {
+  body.classList.toggle("darkMode");
+  localStorage.getItem('darkMode') == 'true'
+    ?
+      localStorage.setItem('darkMode', 'false')
+    :
+      localStorage.setItem('darkMode', 'true');
+}
+
+slider.addEventListener("click", changeTheme);
